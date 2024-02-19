88c9cbc75de235a3d94b761d6e0c16861fe4884f
ClausQAEnerginet <xclpe@energinet.dk>
2024-02-19 08:03:13 +0100

    Simple menu

---
+++ CardboardHoarder/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardboardHoarder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GridSearchAndFilter.Visibility = Visibility.Hidden;
+            GridMyCollection.Visibility = Visibility.Hidden;
+        }
+        private void reset_grids()
+        {
+            GridSearchAndFilter.Visibility = Visibility.Hidden;
+            GridMyCollection.Visibility = Visibility.Hidden;
+        }
+        private void MenuSearchAndFilter_Click(object sender, RoutedEventArgs e)
+        {
+            reset_grids();
+            GridSearchAndFilter.Visibility = Visibility.Visible;
+        }
+
+        private void MenuMyCollection_Click(object sender, RoutedEventArgs e)
+        {
+            reset_grids();
+            GridMyCollection.Visibility = Visibility.Visible;
+        }
+    }
+}
