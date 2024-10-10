d882b58dbde21f07a20537d88a60fc14878d183b
ClausQAEnerginet <xclpe@energinet.dk>
2024-10-10 07:47:55 +0200

    update

--- CollectaMundo/FilterContext.cs
+++ CollectaMundo/FilterContext.cs
@@ -7,12 +7,14 @@
         public List<string> AllSuperTypes { get; set; } = [];
         public List<string> AllSubTypes { get; set; } = [];
         public List<string> AllKeywords { get; set; } = [];
+        public List<string> AllFinishes { get; set; } = [];
 
         public HashSet<string> SelectedColors { get; set; } = [];
         public HashSet<string> SelectedTypes { get; set; } = [];
         public HashSet<string> SelectedSuperTypes { get; set; } = [];
         public HashSet<string> SelectedSubTypes { get; set; } = [];
         public HashSet<string> SelectedKeywords { get; set; } = [];
+        public HashSet<string> SelectedFinish { get; set; } = [];
 
         // Filter defaults
         public string RulesTextDefaultText { get; } = "Filter rulestext...";
@@ -20,6 +22,7 @@
         public string SuperTypesDefaultText { get; } = "Filter supertypes...";
         public string SubTypesDefaultText { get; } = "Filter subtypes...";
         public string KeywordsDefaultText { get; } = "Filter keywords...";
+        public string FinishesDefaultText { get; } = "Filter card finishes...";
 
     }
 
