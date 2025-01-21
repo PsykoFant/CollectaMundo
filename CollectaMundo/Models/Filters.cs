0d4c5e885daae873dc84cfa31fe2b89929bf282e
ClausQAEnerginet <xclpe@energinet.dk>
2025-01-21 05:57:57 +0100

    nak filterselections

---
+++ CollectaMundo/Models/Filters.cs
@@ -0,0 +1,15 @@
+﻿using static CollectaMundo.MainWindow;
+
+namespace CollectaMundo.Models
+{
+    public class Filters
+    {
+        public String? CriteriaKey { get; set; } = null;
+    }
+    public class FilterSelections : Filters
+    {
+        public OperatorType Operator { get; set; } = 0;
+        public String? SingleCriteria { get; set; } = null;
+        public HashSet<string> MultipleCriteria { get; set; } = [];
+    }
+}
