617d7117dc8a496160e606a2373515a6b9982749
ClausQAEnerginet <xclpe@energinet.dk>
2025-04-03 06:44:23 +0200

    update

--- CollectaMundo/Behaviors/DelayedUpdateBehavior.cs
+++ CollectaMundo/Behaviors/DelayedUpdateBehavior.cs
@@ -9,37 +9,36 @@ namespace CollectaMundo.Behaviors
         // Delay in milliseconds – configurable via XAML.
         public int Delay { get; set; } = 500;
 
-        private DispatcherTimer _timer;
+        private DispatcherTimer? _timer;
 
         protected override void OnAttached()
         {
             base.OnAttached();
             AssociatedObject.TextChanged += AssociatedObject_TextChanged;
             _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(Delay) };
-            _timer.Tick += Timer_Tick;
+            _timer!.Tick += Timer_Tick;
         }
 
         protected override void OnDetaching()
         {
             AssociatedObject.TextChanged -= AssociatedObject_TextChanged;
-            _timer.Stop();
-            _timer.Tick -= Timer_Tick;
+            _timer?.Stop();
+            _timer!.Tick -= Timer_Tick;
             base.OnDetaching();
         }
 
         private void AssociatedObject_TextChanged(object sender, TextChangedEventArgs e)
         {
-            // Restart the timer with each keystroke.
-            _timer.Stop();
-            _timer.Start();
+            _timer?.Stop();
+            _timer?.Start();
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void Timer_Tick(object? sender, EventArgs e)
         {
-            _timer.Stop();
-            // Update the binding source after the delay.
+            _timer?.Stop();
             var binding = AssociatedObject.GetBindingExpression(TextBox.TextProperty);
             binding?.UpdateSource();
         }
     }
+
 }
