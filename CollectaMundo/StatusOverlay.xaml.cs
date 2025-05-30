3cebbcf3a8f35ddcf21afc82ef75815393028cee
ClausQAEnerginet <xclpe@energinet.dk>
2025-05-30 09:12:44 +0200

    temp save

---
+++ CollectaMundo/StatusOverlay.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CollectaMundo
+{
+    /// <summary>
+    /// Interaction logic for StatusOverlay.xaml
+    /// </summary>
+    public partial class StatusOverlay : UserControl
+    {
+        public StatusOverlay()
+        {
+            InitializeComponent();
+        }
+    }
+}
