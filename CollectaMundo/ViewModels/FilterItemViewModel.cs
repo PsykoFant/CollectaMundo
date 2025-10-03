55b507ff061d41fdb53da0777273c6f1f0905fe8
ClausQAEnerginet <xclpe@energinet.dk>
2025-10-03 15:07:05 +0200

    update

--- CollectaMundo/ViewModels/FilterItemViewModel.cs
+++ CollectaMundo/ViewModels/FilterItemViewModel.cs
@@ -4,7 +4,6 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Timers;
 using System.Windows;
 using System.Windows.Input;
@@ -287,8 +286,6 @@ namespace CollectaMundo.ViewModels
         private readonly FilterViewModel _filterViewModel;
         public FilterItemViewModel(string criteriaKey, IEnumerable<FilterOption> filterOptions, string defaultText, string readableLabel, FilterViewModel filterViewModel, IEnumerable<int>? numericOptions = null)
         {
-            Debug.WriteLine("FilterItemViewModel created: " + criteriaKey);
-
             _filterViewModel = filterViewModel ?? throw new ArgumentNullException(nameof(filterViewModel));
             CriteriaKey = criteriaKey;
             DefaultText = defaultText;
