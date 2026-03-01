91105d497ab113cefa4c4ab7dc87be10dfb9528d
PsykoFant <spammer@cpnet.dk>
2026-03-01 17:33:50 +0100

    update

---
+++ CollectaMundo/ViewModels/Pages/CardListPageViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectaMundo.ViewModels.Pages
+{
+    // Base wrapper VM for pages that display a primary card list/grid and share common sub-viewmodels.
+    public abstract class CardListPageViewModelBase(CardViewModel cardsVM, CardImageViewModel cardImageVM, FilterViewModel filterVM, ObservableCollection<ObservableCollection<double>> columnWidths, PricesViewModel? pricesVM = null, EditCollectionViewModel? addOrEditCardsVM = null)
+    {
+        // Non-nullable VMs for features that are present on all pages.
+        public CardViewModel CardsVM { get; } = cardsVM;
+        public CardImageViewModel CardImageVM { get; } = cardImageVM;
+        public FilterViewModel FilterVM { get; } = filterVM;
+        public ObservableCollection<ObservableCollection<double>> ColumnWidths { get; } = columnWidths;
+
+        // Nulllable VMs for features that may not be present on all pages.
+        public EditCollectionViewModel? AddCardsVM { get; } = addOrEditCardsVM;
+        public PricesViewModel? PricesVM { get; } = pricesVM;
+    }
+}
--- CollectaMundo/ViewModels/Pages/SearchAndFilterPageViewModel.cs
+++ CollectaMundo/ViewModels/Pages/SearchAndFilterPageViewModel.cs
@@ -3,16 +3,11 @@ using System.Collections.ObjectModel;
 
 namespace CollectaMundo.ViewModels.Pages
 {
-    public sealed class SearchAndFilterPageViewModel(CardViewModel allCardsVM,EditCollectionViewModel addCardsVM,FilterViewModel filterVM,PricesViewModel pricesVM,CardImageViewModel cardImageVM,ObservableCollection<ObservableCollection<double>> columnWidths)
+    public sealed class SearchAndFilterPageViewModel(CardViewModel allCardsVM, EditCollectionViewModel addCardsVM, FilterViewModel filterVM, PricesViewModel pricesVM, CardImageViewModel cardImageVM, ObservableCollection<ObservableCollection<double>> columnWidths) :
+        CardListPageViewModelBase(cardsVM: allCardsVM, cardImageVM: cardImageVM, filterVM: filterVM, columnWidths: columnWidths, pricesVM: pricesVM, addOrEditCardsVM: addCardsVM)
     {
-        public CardViewModel AllCardsVM { get; } = allCardsVM;
-        public EditCollectionViewModel AddCardsVM { get; } = addCardsVM;
-        public FilterViewModel FilterVM { get; } = filterVM;
-        public PricesViewModel PricesVM { get; } = pricesVM;
-        public CardImageViewModel CardImageVM { get; } = cardImageVM;
 
-        public ObservableCollection<ObservableCollection<double>> ColumnWidths { get; } = columnWidths;
-
+        // Convenience header models for the custom filter headers
         public FilterHeaderModel NameHeader => new("Name", FilterVM.Filters["Name"], ColumnWidths[0][0]);
         public FilterHeaderModel SetNameHeader => new("Set Name", FilterVM.Filters["SetName"], ColumnWidths[0][1]);
     }
