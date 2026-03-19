c47925f7ac8f94e8f8c075f40b7336c1fcc57879
PsykoFant <spammer@cpnet.dk>
2026-03-19 23:14:26 +0100

    update

---
+++ CollectaMundo/Views/SideMenuLeft/SideMenuUtilitiesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CollectaMundo.Views.SideMenuLeft
+{
+    /// <summary>
+    /// Interaction logic for UtilitiesSideMenuView.xaml
+    /// </summary>
+    public partial class SideMenuUtilitiesView : UserControl
+    {
+        public SideMenuUtilitiesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
