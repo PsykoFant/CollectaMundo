75b791f1170d8c7e2adee2e089ddaaf355bf7b02
PsykoFant <spammer@cpnet.dk>
2026-06-24 07:35:57 +0200

    enum whatever

---
+++ CollectaMundo/ViewModels/Shell/Models/ShellPageEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace CollectaMundo.ViewModels.Shell.Models
+{
+    public enum ShellPageEnum
+    {
+        None,
+        SearchAndFilter,
+        MyCollection,
+        Decks,
+        Utilities
+    }
+}
