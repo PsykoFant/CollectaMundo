7e315f0da0048861d860514415ab533845e3262f
Claus Petersen <299060888+claus-petersen_lego@users.noreply.github.com>
2026-08-10 07:53:04 +0200

    update

---
+++ CollectaMundo/Presentation/Decks/DeckBuilderTooltips.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectaMundo.Presentation.Decks
+{
+    public static class DeckBuilderTooltips
+    {
+        public const string Owned = "Cards you own in your collection.";
+
+        public const string Available = "Available to this deck from your collection (owned minus copies allocated to other decks).";
+
+        public const string Allocated = "Copies from your collection allocated to this deck.";
+
+        public const string DesiredQuantity = "Cards in this deck.";
+
+        public const string IllegalCard = "This card is not legal in the selected format.";
+
+        public const string InsufficientAvailableQuantity = "Desired quantity exceeds the number of copies available to this deck.";
+    }
+}
